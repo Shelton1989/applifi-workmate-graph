497529f196cf3379381864f11cb0ecdefe640d19
shelton wilson <sheltonwlsn@gmail.com>
2023-02-13 09:50:47 +0100

    feat(tr-be): update schema to make uuid nullable and non unique

--- prisma/generated/type-graphql/resolvers/inputs/UserWhereUniqueInput.d.ts
+++ prisma/generated/type-graphql/resolvers/inputs/UserWhereUniqueInput.d.ts
@@ -1,6 +1,5 @@
 export declare class UserWhereUniqueInput {
     uid?: string | undefined;
-    uuid?: string | undefined;
     id?: string | undefined;
     username?: string | undefined;
     email?: string | undefined;
--- prisma/generated/type-graphql/resolvers/inputs/UserWhereUniqueInput.js
+++ prisma/generated/type-graphql/resolvers/inputs/UserWhereUniqueInput.js
@@ -16,12 +16,6 @@ tslib_1.__decorate([
         nullable: true
     }),
     tslib_1.__metadata("design:type", String)
-], UserWhereUniqueInput.prototype, "uuid", void 0);
-tslib_1.__decorate([
-    TypeGraphQL.Field(_type => String, {
-        nullable: true
-    }),
-    tslib_1.__metadata("design:type", String)
 ], UserWhereUniqueInput.prototype, "id", void 0);
 tslib_1.__decorate([
     TypeGraphQL.Field(_type => String, {
